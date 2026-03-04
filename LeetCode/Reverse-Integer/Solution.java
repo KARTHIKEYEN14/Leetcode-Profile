140bb5619f92ad2e7584f6cd9d21548ce37ecac7
KARTHIKEYEN14 <karthikn1466@gmail.com>
2026-03-04 05:48:18 +0530

    Add solution for Reverse Integer

--- LeetCode/Reverse-Integer/Solution.java
+++ LeetCode/Reverse-Integer/Solution.java
@@ -1,25 +1,19 @@
-1class Solution {
-2    public int reverse(int x) {
-3        int temp = 0;
-4        if(x<0){
-5            temp = -(x);
-6        }
-7        else{
-8            temp = x;
-9        }
-10        long rev = 0;
-11        while(temp > 0){
-12            int rem = temp%10;
-13            rev = (rev*10)+rem;
-14            if(rev>2147483647){
-15                return 0;
-16            }
-17            temp = temp/10;
-18        }
-19        int res = (int) rev;
-20        if(x<0){
-21            return -(res);
-22        }
-23        return res;
-24    }
-25}
+1class Solution:
+2    def reverse(self, x: int) -> int:
+3        if(x<0):
+4            temp = -(x)
+5            reverse = 0 
+6            while(temp!=0):
+7                rem = temp%10
+8                reverse = reverse*10 + rem
+9                temp //= 10
+10            reverse = -(reverse)
+11        else:
+12            temp = x
+13            reverse = 0 
+14            while(temp!=0):
+15                rem = temp%10
+16                reverse = reverse*10 + rem
+17                temp //= 10
+18        result = reverse if ((-2**31)< reverse < ((2**31)-1)) else 0
+19        return result
